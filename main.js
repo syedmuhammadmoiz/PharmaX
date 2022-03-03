1d7781993024eb75dc2c3797ddbceee55d2f8772
syedmuhammadmoiz <moiz66.smm@gmail.com>
2022-03-03 10:49:00 +0500

    merged project

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const path = require("path");
 const url = require("url");
-const { app, BrowserWindow } = require("electron");
+const sql = require("mssql");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
 app.isPackaged || require("electron-reloader")(module);
 
 let mainWindow;
 
 let isDev = false;
+
+const sqlConfig = {
+  user: "sa",
+  password: "1234567890",
+  database: "Versale",
+  server: "localhost",
+  options: {
+    trustedConnection: true,
+    encrypt: true,
+    enableArithAbort: true,
+    trustServerCertificate: true,
+  },
+};
 
 if (
   process.env.NODE_ENV !== undefined &&
@@ -71,6 +85,65 @@ function createMainWindow() {
 }
 
 app.on("ready", createMainWindow);
+
+ipcMain.on("search", (event, arg) => {
+  var conn = new sql.ConnectionPool(sqlConfig);
+  conn
+    .connect()
+    .then(function () {
+      var request = new sql.Request(conn);
+      request
+        .query(`select * from Stock where Name like '${arg}%'`)
+        .then(function (recordset) {
+          mainWindow.webContents.send("search", recordset.recordset);
+          conn.close();
+        })
+        .catch(function (err) {
+          console.log(err);
+          conn.close();
+        });
+      console.log("connection is created");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+ipcMain.on("runqurey", (event, arg) => {
+  const object = arg.map((o) => o.Code);
+  let qurey = `update Medicine  set packing = packing-1 where Code in ('${object.join(
+    "','"
+  )}')`;
+  var conn = new sql.ConnectionPool(sqlConfig);
+  conn.connect().then(function () {
+    var request = new sql.Request(conn);
+    request
+      .query(qurey)
+      .then(function (recordset) {
+        console.log(recordset);
+        conn.close();
+      })
+      .catch(function (err) {
+        console.log(err);
+        conn.close();
+      });
+    var request = new sql.Request(conn);
+    qurey = `update Medicine  set packing = packing-1 where Code in (''${object.join(
+      "'',''"
+    )}'')`;
+    const newqurey = `insert into Qurey (quries) values ('${qurey}')`;
+    console.log(newqurey);
+    request
+      .query(newqurey)
+      .then(function (recordset) {
+        conn.close();
+      })
+      .catch(function (err) {
+        console.log(err);
+        conn.close();
+      });
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
--- src/components/Invoice/InputTable/Invoice/Table/Table.js
+++ src/components/Invoice/InputTable/Invoice/Table/Table.js
@@ -1,7 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { ipcRenderer } from "electron";
 import "./Table.css";
 
 const Table = ({ modalToggle }) => {
+  const [show, setshow] = useState(false); //show model
+  const [search, setsearch] = useState(""); //search input
+  var [data, setdata] = useState([]); //data from database
+  var [invoice, setinvoice] = useState([]); //invoice data
+  var [currentdata, setcurrentdata] = useState({}); //current data
+  const [quantity, setquantity] = useState(null); //quantity of medicine
+  const ref = useRef();
+  const reset = {
+    Bonus: "",
+    Code: "",
+    STP: "",
+    TP: "",
+    Price: "",
+    Quantity: "",
+    Total: "",
+    Name: "",
+    Batch: "",
+  };
+
+  //handle the next input move if input is empty it can't move
+  const handleEnter1 = (event) => {
+    if (event.key.toLowerCase() === "enter") {
+      if (event.target.value.length > 0) {
+        if (search != "") {
+          setshow(true);
+          ipcRenderer.send("search", search);
+        } else {
+          setdata([]);
+        }
+
+        // const form = event.target.form;
+        // const index = [...form].indexOf(event.target);
+        // form.elements[index + 1].focus();
+        // event.preventDefault();
+      }
+    }
+  };
+
+  //handle the Enter event move to the next input
+  const handleEnter = (event) => {
+    if (event.key.toLowerCase() === "enter") {
+      const form = event.target.form;
+      const index = [...form].indexOf(event.target);
+      form.elements[index + 1].focus();
+      event.preventDefault();
+    }
+  };
+
+  //search the input in the database
+  const searchindatabase = (e) => {
+    setsearch(e.target.value);
+  };
+  //get the data from the database and send it to the table
+  ipcRenderer.on("search", (event, arg) => {
+    setdata(arg);
+  });
+
+  //generating the invoice
+  const senddatatoinvoice = (item) => {
+    setshow(!show);
+    setcurrentdata(item);
+    setsearch(item.Code);
+    ref.current?.focus();
+  };
+  //generating invoice
+  const putdataintoinvoice = (e) => {
+    if (e.key.toLowerCase() === "enter") {
+      setinvoice((invoice) => [...invoice, currentdata]);
+      setcurrentdata(reset);
+      console.log(currentdata);
+    }
+  };
+
+  //add Quantity to the invoice useState
+  const addquantity = (e) => {
+    e.preventDefault();
+    setquantity(e.target.value);
+  };
+  // save invoice to database
+  const runqurey = () => {};
+  useEffect(() => {
+    console.log(currentdata.length);
+    console.log(currentdata);
+    console.log("here");
+  }, [currentdata]);
+
   return (
     <div className="input_table">
       <table>
@@ -15,35 +102,93 @@ const Table = ({ modalToggle }) => {
                 className="input-same-2"
                 type="text"
                 name="name"
-                onClick={modalToggle}
+                id="Code"
+                onKeyDown={modalToggle}
+                value={search}
+                onChange={(e) => {
+                  searchindatabase(e);
+                }}
               />
             </th>
             <th>
-              <input className="input-same-3" type="text" name="name" />
+              <input
+                className="input-same-3"
+                type="text"
+                name="name"
+                id="name"
+                onKeyDown={handleEnter}
+                value={currentdata.Name}
+              />
             </th>
             <th>
-              <input className="input-same-2" type="text" name="name" />
+              <input
+                className="input-same-2"
+                type="text"
+                name="name"
+                value={currentdata.Batch}
+                onKeyDown={handleEnter}
+              />
             </th>
             <th>
-              <input className="input-same-1" type="text" name="name" />
+              <input
+                className="input-same-1"
+                type="text"
+                name="name"
+                id="SPrice"
+                value={currentdata.TP}
+              />
             </th>
             <th>
-              <input className="input-same-1" type="text" name="name" />
+              <input
+                className="input-same-1"
+                type="text"
+                name="name"
+                id="Quantity"
+                value={quantity}
+                onChange={(e) => {
+                  addquantity(e);
+                }}
+                onKeyDown={handleEnter}
+              />
             </th>
             <th>
-              <input className="input-same-1" type="text" name="name" />
+              <input
+                className="input-same-1"
+                type="text"
+                name="name"
+                id="Bonus"
+                onKeyDown={handleEnter}
+                value={currentdata.length == 0 ? "" : currentdata.Bonus}
+              />
             </th>
             <th>
-              <input className="input-same-1" type="text" name="name" />
+              <input
+                className="input-same-1"
+                type="text"
+                name="name"
+                id="saleTax"
+                onKeyDown={handleEnter}
+                value={currentdata.length == 0 ? "" : currentdata.STP}
+              />
             </th>
             <th>
-              <input className="input-same-1" type="text" name="name" />
+              <input
+                className="input-same-1"
+                type="text"
+                name="name"
+                id="Bonous"
+                onKeyDown={handleEnter}
+                value={currentdata.Bonus}
+              />
             </th>
             <th>
               <input
                 className="input-same-2 last_input"
                 type="text"
                 name="name"
+                id="Total amount"
+                value={quantity != null ? quantity * currentdata.TP : ""}
+                onKeyDown={putdataintoinvoice}
               />
             </th>
           </tr>
